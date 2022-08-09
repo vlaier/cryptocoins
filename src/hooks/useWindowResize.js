2f214216bbf0417a58436877a827ee9a578f0201
vlaier <piotrzielu.1996@gmail.com>
2022-08-09 23:31:11 +0200

    Add useWIndowResize hook

---
+++ src/hooks/useWindowResize.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+export default function useWindowResize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
