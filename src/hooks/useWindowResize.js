8375eb76cec8a5d134b26eb57ec98b7bab91d2a7
vlaier <piotrzielu.1996@gmail.com>
2022-08-10 10:57:26 +0200

    Fix bug in useWindowResize

--- src/hooks/useWindowResize.js
+++ src/hooks/useWindowResize.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 export default function useWindowResize() {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
+  const [windowSize, setWindowSize] = useState(false);
+
   useEffect(() => {
+    !windowSize &&
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
