de5af563dd38a40a5c9f084a0a63274bfec320e6
vlaier <piotrzielu.1996@gmail.com>
2022-08-12 22:34:05 +0200

    Add loading news to API calls

--- src/lib/apiCallls.js
+++ src/lib/apiCallls.js
@@ -1,6 +1,6 @@
 export async function loadCoins() {
   const res = await fetch(
-    "https://api.coinstats.app/public/v1/coins?skip=0&limit=20"
+    "https://api.coinstats.app/public/v1/coins?skip=0&limit=50"
   );
   const data = await res.json();
   return data;
@@ -8,3 +8,10 @@ export async function loadCoins() {
 // export async function loadCoin(){
 //   const res =  await fetch(`https://api.coinstats.app/public/v1/coins/${params.id}`)
 // }
+export async function loadNews() {
+  const res = await fetch(
+    "https://api.coinstats.app/public/v1/news/trending?skip=0&limit=20"
+  );
+  const data = await res.json();
+  return data;
+}
